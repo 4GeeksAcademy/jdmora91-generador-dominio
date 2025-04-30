eda7c8f585745f827efde3c7b322512392ad1f2b
Jose Mora <j.donismora@gmail.com>
2025-04-30 21:32:43 +0000

    usoDeInglesEnVariable,cambioLetPorCons,usoDeString

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  console.log("Hola clase de arreglos");
+
+  const animals = ["perro", "gato", "loro", "tortuga", "pez"];
+  const places = ["casa", "oficina", "parque", "playa", "montaña"];
+  const qualities = ["lindo", "feo", "grande", "pequeño", "rápido"];
+  const extensions = [".com", ".es", ".org", ".net", ".io"];
+
+  for (const animal of animals) {
+    for (const place of places) {
+      for (const quality of qualities) {
+        for (const extension of extensions) {
+          console.log(`${animal}${place}${quality}${extension}`);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
